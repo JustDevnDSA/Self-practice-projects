82c01334d647fcb7466bdf2102e49c570b7f7e24
DevnDSA <132866106+JustDevnDSA@users.noreply.github.com>
2024-06-03 22:16:47 +0530

    Add files via upload

---
+++ 5_TechTrack Watchlist/script.js
@@ -0,0 +1,18 @@
+const toggleMenu = () => {
+  navMobile.classList.toggle("hidden");
+  bars.classList.toggle("fa-xmark");
+};
+
+
+
+// close mobile nav after click on any link
+// function hideMobileMenu() {
+//   document.getElementById("navMobile").classList.add("hidden");
+// }
+
+// document.getElementById('jlink').addEventListener('click', hideMobileMenu);
+// document.getElementById('blink').addEventListener('click', hideMobileMenu);
+// document.getElementById('flink').addEventListener('click', hideMobileMenu);
+// document.getElementById('balink').addEventListener('click', hideMobileMenu);
+// document.getElementById('elink').addEventListener('click', hideMobileMenu);
+// document.getElementById('plink').addEventListener('click', hideMobileMenu);
